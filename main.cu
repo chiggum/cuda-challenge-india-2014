96cf429ba9c43e050ea08a16abab759839618b11
chiggum <codechiggum@gmail.com>
2014-10-05 12:49:18 +0530

    removed some recopying code and converted everything into one function

--- main.cu
+++ main.cu
@@ -14,97 +14,6 @@
 
 typedef unsigned int uint;
 
-__global__ void assignNewValuesParallel(uint *map, uint r, uint c, uint *d_map, bool *notConverged) {
-	uint idx = threadIdx.x + blockIdx.x*blockDim.x;
-	uint i = idx/c;
-	uint j = idx%c;
-	if(i >= r || j >= c)
-		return;
-	uint focusElem = map[j+i*c];
-	if(i == 0 || i == r - 1 || j == 0 || j == c - 1) {
-		d_map[j + i*c] = focusElem;
-		return;
-	}
-	uint localMap[8];
-	int cntU = 0;
-	int cntD = 0;
-	int cnt = 0;
-	for(int l = i - 1; l <= i + 1; ++l) {
-		for(int k = j - 1; k <= j + 1; ++k) {
-			if(!(l == i && k == j)) {
-				localMap[cnt] = map[k+l*c];
-				if(localMap[cnt] > focusElem)
-					++cntD;
-				else if(localMap[cnt] < focusElem)
-					++cntU;
-				++cnt;
-			}
-		}
-	}
-	if(cntU == 8) {
-		uint sum = 0;
-		for(int l = 0; l < 8; ++l)
-			sum+=localMap[l];
-		sum /= 8;
-		d_map[j + i * c]=sum;
-	} else if(cntD == 8) {
-		for(int l = 0; l < 7; ++l) {
-			for(int k = 0; k < 7 - l; ++k) {
-				if(localMap[k] > localMap[k+1]) {
-					uint temp = localMap[k];
-					localMap[k] = localMap[k+1];
-					localMap[k+1]=temp;
-				}
-			}
-		}
-		d_map[j + i * c] = (localMap[3]+localMap[4])/2;
-	} else {
-		d_map[j+i*c] = focusElem;
-	}
-	if(d_map[j+i*c] != focusElem)
-		*notConverged = false;
-}
-
-void saturateMapParallel(uint *map, uint rows, uint cols) {
-	int iter = 0;
-	bool *notConverged = new bool[1];
-	notConverged[0]=true;
-	long long int total = rows*cols;
-	dim3 threadsPerBlock(1024);
-	dim3 numBlocks((total-1)/threadsPerBlock.x + 1); 
-	while(iter < NUM_ITERATIONS && notConverged[0]) {
-		uint *d_map_out, *d_map_in;
-		cudaMalloc((void**)&d_map_in, rows*cols*sizeof(uint));
-		cudaMalloc((void**)&d_map_out, rows*cols*sizeof(uint));
-		cudaMemcpy(d_map_in, map, rows*cols*sizeof(uint), cudaMemcpyHostToDevice);
-		bool *d_isCvg;
-		cudaMalloc((void**)&d_isCvg, sizeof(bool));
-		cudaMemcpy(d_isCvg, notConverged, sizeof(bool), cudaMemcpyHostToDevice);
-		assignNewValuesParallel<<<numBlocks, threadsPerBlock>>>(d_map_in, rows, cols, d_map_out, d_isCvg);
-		cudaDeviceSynchronize();
-		cudaMemcpy(notConverged, d_isCvg, sizeof(bool), cudaMemcpyDeviceToHost);
-		notConverged[0] = !notConverged[0];
-		cudaMemcpy(map, d_map_out, rows*cols*sizeof(uint), cudaMemcpyDeviceToHost);
-		cudaFree(d_map_in);
-		cudaFree(d_map_out);
-		cudaFree(d_isCvg);
-		iter++;
-	}
-	delete notConverged;
-}
-
-
-
-double calcThreshold(uint *map, uint r, uint c) {
-	uint *raw_ptr; 
-	cudaMalloc((void **) &raw_ptr, r*c *sizeof(uint));
-	cudaMemcpy(raw_ptr, map, r*c*sizeof(uint), cudaMemcpyHostToDevice);
-	thrust::device_ptr<uint> dev_ptr(raw_ptr);
-	long long int sum = thrust::reduce(dev_ptr, dev_ptr+r*c, (uint) 0, thrust::plus<uint>());
-	cudaFree(raw_ptr);
-	return sum/(r*c);
-}
-
 __global__ void assignNewValCmpThresh(uint *map, uint r, uint c, uint *d_in, uint T) {
 	uint idx = threadIdx.x + blockIdx.x*blockDim.x;
 	uint i = idx/c;
@@ -115,27 +24,6 @@ __global__ void assignNewValCmpThresh(uint *map, uint r, uint c, uint *d_in, uin
 		d_in[j+i*c]=0;
 	else
 		d_in[j+i*c]=1;
-}
-
-void binarize(uint *map, uint rows, uint cols, uint T) {
-	long long int total = rows*cols;
-	dim3 threadsPerBlock(1024);
-	dim3 numBlocks((int)((total-1)/threadsPerBlock.x) + 1); 
-	uint *d_map_out, *d_map_in;
-	cudaMalloc((void**)&d_map_in, rows*cols*sizeof(uint));
-	cudaMalloc((void**)&d_map_out, rows*cols*sizeof(uint));
-	cudaMemcpy(d_map_in, map, rows*cols*sizeof(uint), cudaMemcpyHostToDevice);
-	assignNewValCmpThresh<<<numBlocks, threadsPerBlock>>>(d_map_in, rows, cols, d_map_out, T);
-	cudaDeviceSynchronize();
-	cudaMemcpy(map, d_map_out, rows*cols*sizeof(uint), cudaMemcpyDeviceToHost);
-	cudaFree(d_map_in);
-	cudaFree(d_map_out);
-}
-
-void convertToBinaryMap(uint *map, uint rows, uint cols) {
-	uint T = calcThreshold(map, rows, cols);
-	//std::cout << "Threshold " << T << std::endl;
-	binarize(map, rows, cols, T);
 }
 
 __global__ void assignLabels(int *d_label, uint *d_map, uint r, uint c) {
@@ -227,55 +115,122 @@ __global__ void makeValuesOne(uint *d_map, int *d_label, uint r, uint c) {
 		d_map[j+i*c]=1;
 }
 
-void calcNPrintNumConnCompParallel(uint *map, uint rows, uint cols) {
-	bool *notConverged= new bool[1];
-	notConverged[0] = true;
-	//asign labels
-	int *d_label;
-	uint *d_map_in;
-	uint r_ = rows+2;
-	uint c_ = cols+2;
-	cudaMalloc((void**)&d_map_in, rows*cols*sizeof(uint));
-	cudaMemcpy(d_map_in, map, rows*cols*sizeof(uint), cudaMemcpyHostToDevice);
-	cudaMalloc((void**)&d_label, r_*c_*sizeof(int));
-	cudaMemset(d_label, 0, r_*c_*sizeof(int));
-	long long int total = rows*cols;
-	dim3 threadsPerBlock(1024);
-	dim3 numBlocks((int)((total-1)/threadsPerBlock.x) + 1);
-	assignLabels<<<numBlocks, threadsPerBlock>>>(d_label, d_map_in, rows, cols); 
-	cudaDeviceSynchronize();
-	while(notConverged[0]) {
-		bool *d_isCvg;
-		cudaMalloc((void**)&d_isCvg, sizeof(bool));
-		cudaMemcpy(d_isCvg, notConverged, sizeof(bool), cudaMemcpyHostToDevice);
-		assignMinSurrLabel<<<numBlocks, threadsPerBlock>>>(d_label, d_isCvg, rows, cols);
-		cudaDeviceSynchronize();
-		cudaMemcpy(notConverged, d_isCvg, sizeof(bool), cudaMemcpyDeviceToHost);
-		notConverged[0] = !notConverged[0];
-		cudaFree(d_isCvg);
-		if(notConverged[0]) {
-			analysisPhase<<<numBlocks, threadsPerBlock>>>(d_label, rows, cols);
-			cudaDeviceSynchronize();
-			//labellingPhase<<<numBlocks, threadsPerBlock>>>(d_map_in, d_label, d_r_label, rows, cols);
-			//cudaDeviceSynchronize();
+__global__ void assignNewValuesParallel(uint *map, uint r, uint c, uint *d_map, bool *notConverged) {
+	uint idx = threadIdx.x + blockIdx.x*blockDim.x;
+	uint i = idx/c;
+	uint j = idx%c;
+	if(i >= r || j >= c)
+		return;
+	uint focusElem = map[j+i*c];
+	if(i == 0 || i == r - 1 || j == 0 || j == c - 1) {
+		d_map[j + i*c] = focusElem;
+		return;
+	}
+	uint localMap[8];
+	int cntU = 0;
+	int cntD = 0;
+	int cnt = 0;
+	uint sum = 0;
+	for(int l = i - 1; l <= i + 1; ++l) {
+		for(int k = j - 1; k <= j + 1; ++k) {
+			if(!(l == i && k == j)) {
+				localMap[cnt] = map[k+l*c];
+				sum += localMap[cnt];
+				if(localMap[cnt] > focusElem)
+					++cntD;
+				else if(localMap[cnt] < focusElem)
+					++cntU;
+				++cnt;
+			}
 		}
 	}
-	
-
-	makeValuesOne<<<numBlocks, threadsPerBlock>>>(d_map_in, d_label, rows, cols);
-	cudaDeviceSynchronize();
-	thrust::device_ptr<uint> dev_ptr(d_map_in);
-	uint sum = thrust::reduce(dev_ptr, dev_ptr+rows*cols, (uint) 0, thrust::plus<uint>());
-	std::cout << sum << std::endl;
-	cudaFree(d_map_in);
-	cudaFree(d_label);
+	if(cntU == 8) {
+		d_map[j + i * c]=sum/8;
+		*notConverged = false;
+	} else if(cntD == 8) {
+		for(int l = 0; l < 4; ++l) {
+			for(int k = 0; k < 7 - l; ++k) {
+				if(localMap[k] > localMap[k+1]) {
+					uint temp = localMap[k];
+					localMap[k] = localMap[k+1];
+					localMap[k+1]=temp;
+				}
+			}
+		}
+		d_map[j + i * c] = (localMap[3]+localMap[4])/2;
+		*notConverged = false;
+	} else {
+		d_map[j+i*c] = focusElem;
+	}
+		
 }
 
 void processNPrintSol(uint *map, uint rows, uint cols) {
-	//saturate map
-	saturateMapParallel(map, rows, cols);
-	convertToBinaryMap(map, rows, cols);
-	calcNPrintNumConnCompParallel(map, rows, cols);
+	int iter = 0;
+	long long int total = rows*cols;
+	dim3 threadsPerBlock(1024);
+	dim3 numBlocks((total-1)/threadsPerBlock.x + 1); 
+	uint *d_map_out, *d_map_in;
+	cudaMalloc((void**)&d_map_in, rows*cols*sizeof(uint));
+	cudaMalloc((void**)&d_map_out, rows*cols*sizeof(uint));
+	cudaMemcpy(d_map_in, map, rows*cols*sizeof(uint), cudaMemcpyHostToDevice);
+	bool *d_isCvg;
+	cudaMalloc((void**)&d_isCvg, sizeof(bool));
+	bool notConverged = true;
+	while(iter < NUM_ITERATIONS && notConverged) {
+		//notConverged = true;
+		cudaMemcpy(d_isCvg, &notConverged, sizeof(bool), cudaMemcpyHostToDevice);
+		assignNewValuesParallel<<<numBlocks, threadsPerBlock>>>(d_map_in, rows, cols, d_map_out, d_isCvg);
+		cudaDeviceSynchronize();
+		cudaMemcpy(&notConverged, d_isCvg, sizeof(bool), cudaMemcpyDeviceToHost);
+		notConverged = !notConverged;
+		++iter;
+		//cudaMemcpy(map, d_map_out, rows*cols*sizeof(uint), cudaMemcpyDeviceToHost);
+		if(iter >= NUM_ITERATIONS || !notConverged) {
+			thrust::device_ptr<uint> dev_ptr(d_map_out);
+			uint sum = thrust::reduce(dev_ptr, dev_ptr+rows*cols, (uint) 0, thrust::plus<uint>());
+			sum = sum / (rows*cols);
+			assignNewValCmpThresh<<<numBlocks, threadsPerBlock>>>(d_map_out, rows, cols, d_map_in, sum);
+			cudaDeviceSynchronize();
+			int *d_label;
+			uint r_ = rows+2;
+			uint c_ = cols+2;
+			cudaMalloc((void**)&d_label, r_*c_*sizeof(int));
+			cudaMemset(d_label, 0, r_*c_*sizeof(int));
+			assignLabels<<<numBlocks, threadsPerBlock>>>(d_label, d_map_in, rows, cols); 
+			cudaDeviceSynchronize();
+			bool notConverged1 = true;
+			bool *d_isCvg1;
+			cudaMalloc((void**)&d_isCvg1, sizeof(bool));
+			while(notConverged1) {
+				//notConverged1 = true;
+				cudaMemcpy(d_isCvg1, &notConverged1, sizeof(bool), cudaMemcpyHostToDevice);
+				assignMinSurrLabel<<<numBlocks, threadsPerBlock>>>(d_label, d_isCvg1, rows, cols);
+				cudaDeviceSynchronize();
+				cudaMemcpy(&notConverged1, d_isCvg1, sizeof(bool), cudaMemcpyDeviceToHost);
+				notConverged1 = !notConverged1;
+				if(notConverged1) {
+					analysisPhase<<<numBlocks, threadsPerBlock>>>(d_label, rows, cols);
+					cudaDeviceSynchronize();
+				}
+			}
+			cudaFree(d_isCvg1);
+			makeValuesOne<<<numBlocks, threadsPerBlock>>>(d_map_in, d_label, rows, cols);
+			cudaDeviceSynchronize();
+			thrust::device_ptr<uint> dev_ptr1(d_map_in);
+			uint sum1 = thrust::reduce(dev_ptr1, dev_ptr1+rows*cols, (uint) 0, thrust::plus<uint>());
+			std::cout << sum1 << std::endl;
+			cudaFree(d_label);
+		} else {
+			thrust::device_ptr<uint> dev_ptr2(d_map_in);
+			thrust::device_ptr<uint> dev_ptr3(d_map_out);
+			thrust::copy(dev_ptr3, dev_ptr3+rows*cols, dev_ptr2);
+			//cudaMemcpy(d_map_in, d_map_out, rows*cols*sizeof(uint), cudaMemcpyDeviceToDevice);
+		}
+	}
+	cudaFree(d_isCvg);
+	cudaFree(d_map_in);
+	cudaFree(d_map_out);
 }
 
 int main() {
